d50570ce11a22778fb92fdd1f254d3b18ea802ef
Kirill Miklashevich <Dizardmk@gmail.com>
2022-09-09 12:15:07 +0300

    adds fix to params php

--- src/app/params.php
+++ src/app/params.php
@@ -15,15 +15,15 @@ if (!empty($_POST)) {
 $ini_array = parse_ini_file('params.ini');
 ?>
 
-<form action="" method="post">
+<form action="" method="post" style="display: flex; flex-direction: column">
 
-    <label for="product_name">Название продукта</label>
-    <input type="text" id="product_name" name="product_name" value="<?= $ini_array[
+    <label for="product_name">Zoho Product Name</label>
+    <input type="text" id="product_name" name="product_name" style="margin-bottom: 10px; max-width: 300px" value="<?= $ini_array[
       'PRODUCT_NAME'
     ] ?>" placeholder="<?= $ini_array['PRODUCT_NAME'] ?>">
 
-    <label for="product_id">ID продукта</label>
-    <input type="text" id="product_id" name="product_id" value="<?= $ini_array[
+    <label for="product_id">Zoho Product ID</label>
+    <input type="text" id="product_id" name="product_id" style="margin-bottom: 10px; max-width: 300px" value="<?= $ini_array[
       'PRODUCT_ID'
     ] ?>" placeholder="<?= $ini_array['PRODUCT_ID'] ?>">
 
@@ -33,17 +33,17 @@ $ini_array = parse_ini_file('params.ini');
     ] ?>" placeholder="<?= $ini_array['GTM'] ?>">
 
     <label for="leeloo_hash">Leeloo Hash</label>
-    <input type="text" id="leeloo_hash" name="leeloo_hash" value="<?= $ini_array[
+    <input type="text" id="leeloo_hash" name="leeloo_hash" style="margin-bottom: 10px; max-width: 300px" value="<?= $ini_array[
       'LEELOO_HASH'
     ] ?>" placeholder="<?= $ini_array['LEELOO_HASH'] ?>">
 
-    <label for="start_date">Дата старта</label>
-    <input type="text" id="start_date" name="start_date" value="<?= $ini_array[
+    <label for="start_date">Start Date</label>
+    <input type="text" id="start_date" name="start_date" style="margin-bottom: 10px; max-width: 300px" value="<?= $ini_array[
       'START_DATE'
     ] ?>" placeholder="<?= $ini_array['START_DATE'] ?>">
 
-    <label for="password">Пароль</label>
-    <input type="password" id="password" name="password">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" style="margin-bottom: 10px; max-width: 300px">
 
-    <button type="submit">Сохранить</button>
+    <button type="submit" style="max-width: 300px">Save</button>
 </form>
