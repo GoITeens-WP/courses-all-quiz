c36fc668bcf4a9dba298bb1a6b91f0573ee450d2
Kirill Miklashevich <Dizardmk@gmail.com>
2022-09-19 15:59:03 +0300

    fix params

--- src/app/components/Params.php
+++ src/app/components/Params.php
@@ -8,11 +8,11 @@ class Params
   {
     if ($this->validatePassword($_POST['password'])) {
       $file = fopen(dirname(__FILE__, 2) . '/params.ini', 'w+');
-      fwrite($file, "PRODUCT_NAME={$_POST['product_name']}\n");
-      fwrite($file, "PRODUCT_ID={$_POST['product_id']}\n");
-      fwrite($file, "GTM={$_POST['gtm']}\n");
-      fwrite($file, "LEELOO_HASH={$_POST['leeloo_hash']}\n");
-      fwrite($file, "START_DATE={$_POST['start_date']}\n");
+      fwrite($file, "PRODUCT_NAME=\"{$_POST['product_name']}\"\n");
+      fwrite($file, "PRODUCT_ID=\"{$_POST['product_id']}\"\n");
+      fwrite($file, "GTM=\"{$_POST['gtm']}\"\n");
+      fwrite($file, "LEELOO_HASH=\"{$_POST['leeloo_hash']}\"\n");
+      fwrite($file, "START_DATE=\"{$_POST['start_date']}\"\n");
       fclose($file);
     } else {
       echo 'Пароль введен не верно!';
