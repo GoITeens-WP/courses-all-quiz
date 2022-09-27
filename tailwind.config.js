cfd112ae90e9bf54c8718fcc5eaa6795a2787a43
Kirill Miklashevich <Dizardmk@gmail.com>
2022-09-27 19:11:52 +0300

    adds editorconfig and some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,46 @@ const { colors } = require(`tailwindcss/defaultTheme`);
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    // MEDIA QUERIES
     screens: {
+      smOnly: {'max': '767.98px'},
       sm: '480px',
+      mdOnly: {'min': '768px', 'max': '1279.98px'},
       md: '768px',
       xl: '1280px',
+      notXl: {'max': '1279.98px'},
     },
+    // BASE FONT
     fontFamily: {
-      montserrat: ['Montserrat', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'], // class="font-montserrat"
+      // exo: ['"Exo 2"', 'sans-serif'],
     },
-
+    // SHADOW
+    boxShadow: {
+      orange: '2px 8px 29px rgba(240, 127, 46, 0.2)', // class="shadow-orange"
+    },
+    // THEME
     extend: {
       backgroundColor: theme => ({
         ...theme('colors'),
-        body: '#FFF8EF',
       }),
+      // ALL COLORS
       colors: {
-        orange: '#FF6C00',
-        black: '#202020',
+        body: '#FFF8EF', // class="bg-body"
+        black: {
+          DEFAULT: '#000000', // class="bg-black text-black border-black"
+          light: "#202020", // class="bg-black-light text-black-light border-black-light"
+          dark: "#010101"
+        },
+        white: {
+          DEFAULT: '#ffffff', // class="bg-white text-white border-white"
+          dark: "#fafafa" // class="bg-white-dark text-white-dark border-white-dark"
+        },
+        accent: '#FF6C00',
+        primary: "#f1f1f1",
+        second: "#f2f2f2",
       },
+      // CONTAINER
       container: {
         center: true,
         padding: {
@@ -29,16 +51,15 @@ module.exports = {
           md: '2rem',
           xl: '2.5rem',
         },
-        sm: '20px',
-        md: '32px',
-        xl: '40px',
       },
+      // KEYFRAMES
       keyframes: {
         side: {
           '0%, 100%': { transform: 'translateX(25%)' },
           '50%': { transform: ' translateY(0)' },
         },
       },
+      // ANIMATION
       animation: {
         side: 'side 1s ease-in-out infinite',
       },
