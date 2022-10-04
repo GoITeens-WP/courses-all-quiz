b4051df93cbf11ddd60b3b6f7ffe646b12fd436d
Dizardmk <Dizardmk@gmail.com>
2022-10-04 17:59:20 +0300

    update modern-normalize

--- build.sh
+++ build.sh
@@ -10,9 +10,5 @@ rm -rf node_modules
 npm i
 npm run build
 
-# Copy app & crm folders
-# cp -r ./src/app ./build/app
-# cp -r ./src/crm ./build/crm
-
 # Remove non-used params.ini file
 rm ./build/app/params.ini
