3af7bb8794b680f4b54ce486bf0ebc020cae9368
Dizardmk <Dizardmk@gmail.com>
2023-08-11 11:10:03 +0300

    feat: add formatting

--- src/check-email.php
+++ src/check-email.php
@@ -7,29 +7,29 @@ echo checkEmail($input['email']);
 
 function checkEmail($email)
 {
-    if (isset($email)) {
-        $domain = substr(strrchr($email, "@"), 1);
+  if (isset($email)) {
+    $domain = substr(strrchr($email, "@"), 1);
 
-        function mxRecordValidate($email, $domain)
-        {
-            $arr = dns_get_record($domain, DNS_MX);
-            if ($arr[0]['host'] == $domain && !empty($arr[0]['target'])) {
-                return $arr[0]['target'];
-            }
-        }
-
-        if (mxRecordValidate($email, $domain)) {
-            $data = dns_get_record($domain, DNS_MX);
-
-            return json_encode([
-                'status' => 'ok',
-                'response' => $data
-            ]);
-        } else {
-            return json_encode([
-                'status' => 'error',
-                'response' => 'This MX records does not exists. Invalid Email Address.'
-            ]);
-        }
+    function mxRecordValidate($email, $domain)
+    {
+      $arr = dns_get_record($domain, DNS_MX);
+      if ($arr[0]['host'] == $domain && !empty($arr[0]['target'])) {
+        return $arr[0]['target'];
+      }
     }
+
+    if (mxRecordValidate($email, $domain)) {
+      $data = dns_get_record($domain, DNS_MX);
+
+      return json_encode([
+        'status' => 'ok',
+        'response' => $data
+      ]);
+    } else {
+      return json_encode([
+        'status' => 'error',
+        'response' => 'This MX records does not exists. Invalid Email Address.'
+      ]);
+    }
+  }
 }
--- src/phpmailer/mail.php
+++ src/phpmailer/mail.php
@@ -25,29 +25,29 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 $mail->CharSet = 'UTF-8';
 
 try {
-	$mail->From = $email;
-	$mail->FromName = $name;
+  $mail->From = $email;
+  $mail->FromName = $name;
 
-	// Получатель письма
-	$mail->addAddress($recipient);
+  // Получатель письма
+  $mail->addAddress($recipient);
 
-	// Прикрипление файла к письму
+  // Прикрипление файла к письму
   $mail->AddAttachment($file_tmp, $file_name);
 
-	// Отправка сообщения
-	$mail->isHTML(false);
-	$mail->Subject = $title;
-	$mail->Body = $body;
+  // Отправка сообщения
+  $mail->isHTML(false);
+  $mail->Subject = $title;
+  $mail->Body = $body;
 
-	// Проверяем отравленность сообщения
-	if ($mail->send()) {
-		$result = 'success';
-	} else {
-		$result = 'error';
-	}
+  // Проверяем отравленность сообщения
+  if ($mail->send()) {
+    $result = 'success';
+  } else {
+    $result = 'error';
+  }
 } catch (Exception $e) {
-	$result = 'error';
-	$status = "The message was not sent. The reason for the error: {$mail->ErrorInfo}";
+  $result = 'error';
+  $status = "The message was not sent. The reason for the error: {$mail->ErrorInfo}";
 }
 
 $mail->ClearAddresses();
@@ -58,13 +58,13 @@ echo json_encode(['result' => $result, 'resultfile' => $rfile, 'status' => $stat
 
 function logRequest($request)
 {
-	$file = fopen('mail.log', 'a+');
-	$date = date(DATE_RFC822);
+  $file = fopen('mail.log', 'a+');
+  $date = date(DATE_RFC822);
 
-	$string = [
-		'date' => $date,
-		'input' => $request,
-	];
-	fwrite($file, json_encode($string, JSON_UNESCAPED_UNICODE) . ',');
-	fclose($file);
+  $string = [
+    'date' => $date,
+    'input' => $request,
+  ];
+  fwrite($file, json_encode($string, JSON_UNESCAPED_UNICODE) . ',');
+  fclose($file);
 }
