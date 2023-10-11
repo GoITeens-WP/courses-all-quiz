c981953c11f1dfcfddbec7b79412eead58364180
Dizardmk <Dizardmk@gmail.com>
2023-10-11 14:26:38 +0300

    feat: add imagesWebp task

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ const watcher = done => {
   );
   watch(paths.watch.js).on('change', series(tasks.scripts, browserSync.reload));
   watch(paths.watch.images, tasks.images);
+  watch(paths.watch.images, tasks.imagesWebp);
   watch(paths.watch.sprite, tasks.sprite);
   watch(paths.watch.fonts, tasks.fonts);
   watch(paths.watch.crm, tasks.crm);
