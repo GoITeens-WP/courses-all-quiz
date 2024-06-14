14e63c27b78393ee0c9ee18bf1e308890ed5e980
MaksymZak <zaksumy1989@gmail.com>
2024-06-14 13:01:14 +0300

    add new webp optimization and add scripts to create starter

--- gulp/tasks/imagesWebp.js
+++ gulp/tasks/imagesWebp.js
@@ -1,51 +1,49 @@
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-const mode = require('gulp-mode')();
 const newer = require('gulp-newer');
+const rename = require('gulp-rename');
+const size = require('gulp-size');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
-const size = require('gulp-size');
-const webp = require('gulp-webp');
 const paths = require('../paths');
+const sharp = require('sharp');
+const through2 = require('through2');
 
 const imagesWebp = () => {
   return gulp
-    .src(['src/assets/images/sections/**/*'])
+    .src([
+      `${paths.src.images}/*.{jpg,png}`,
+      '!src/assets/images/components/favicon/**',
+      '!src/assets/images/components/intTelInput/**',
+      '!src/assets/images/components/ogp/**',
+    ])
     .pipe(
-      plumber({
-        errorHandler: function (error) {
-          // if error in dev mode
-          notify.onError({
-            title: 'IMAGESWEBP',
-            message: 'Error: <%= error.message %>',
-          })(error);
-
-          // if error in production mode
-          if (mode.production()) {
-            console.error(`❌ Error: [IMAGESWEBP] ${error.message}`);
-            process.exit(1);
-          }
-        },
-      })
+      plumber(
+        notify.onError({
+          title: 'IMAGESWEBP',
+          message: 'Error: <%= error.message %>',
+        })
+      )
     )
     .pipe(newer(paths.build.images))
     .pipe(
-      mode.production(
-        imagemin([
-          imagemin.svgo({
-            plugins: [
-              { cleanupIDs: false },
-              { removeViewBox: true },
-              { removeComments: true },
-              { removeEmptyContainers: true },
-            ],
-          }),
-        ])
-      )
+      through2.obj(function (file, _, cb) {
+        if (file.isBuffer() && /\.(jpe?g|png)$/i.test(file.path)) {
+          sharp(file.contents)
+            .webp({ quality: 100 })
+            .toBuffer()
+            .then(data => {
+              file.contents = data;
+              cb(null, file);
+            })
+            .catch(err => cb(err));
+        } else {
+          cb(null, file);
+        }
+      })
     )
-    .pipe(webp())
+    .pipe(rename({ extname: '.webp' }))
     .pipe(size({ showFiles: true }))
-    .pipe(gulp.dest(`${paths.build.images}/sections/`));
+    .pipe(gulp.dest(paths.build.images));
 };
 
 module.exports = imagesWebp;
