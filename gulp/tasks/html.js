cf76209e77baf07ecb9defcf0b5ba78b5bf43d46
Kyryllo Miklashevych <Dizardmk@gmail.com>
2024-11-15 11:58:39 +0200

    feat: add conservativeCollapse to html gulp task

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,20 +1,21 @@
-const gulp = require('gulp');
-const fs = require('fs');
-const cachebust = require('gulp-cache-bust');
-const data = require('gulp-data');
-const htmlmin = require('gulp-htmlmin');
-const mode = require('gulp-mode')();
-const nunjucksRender = require('gulp-nunjucks-render');
-const rename = require('gulp-rename');
-const notify = require('gulp-notify');
-const plumber = require('gulp-plumber');
-const paths = require('../paths');
+const gulp = require("gulp");
+const fs = require("fs");
+const cachebust = require("gulp-cache-bust");
+const data = require("gulp-data");
+const htmlmin = require("gulp-htmlmin");
+const mode = require("gulp-mode")();
+const nunjucksRender = require("gulp-nunjucks-render");
+const rename = require("gulp-rename");
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
+const paths = require("../paths");
 
 // other htmlminConfig settings: https://github.com/kangax/html-minifier#options-quick-reference
 const htmlminConfig = {
   minifyCSS: true, // Minify CSS in style elements and style attributes
   minifyJS: true, // Minify JavaScript in script elements and event attributes
   collapseWhitespace: true, // Collapse white space that contributes to text nodes in a document tree
+  conservativeCollapse: true, // Always collapse to 1 space (never remove it entirely). Must be used in conjunction with collapseWhitespace=true
   removeComments: true, // Strip HTML comments
   removeScriptTypeAttributes: true, // Remove type="text/javascript" from script tags. Other type attribute values are left intact
   removeStyleLinkTypeAttributes: true, // Remove type="text/css" from style and link tags. Other type attribute values are left intact
@@ -22,17 +23,17 @@ const htmlminConfig = {
 };
 
 const cachebustConfig = {
-  type: 'timestamp',
+  type: "timestamp",
 };
 
 const manageEnvironment = function (environment) {
-  environment.addFilter('json', function (value) {
+  environment.addFilter("json", function (value) {
     return JSON.parse(value); // convert the complete string imported by Nunjucks into JSON and return
   });
 };
 
-const getDataForFile = file => {
-  return JSON.parse(fs.readFileSync('./src/json/data.json'));
+const getDataForFile = (file) => {
+  return JSON.parse(fs.readFileSync("./src/json/data.json"));
 };
 
 const html = () => {
@@ -43,8 +44,8 @@ const html = () => {
         errorHandler: function (error) {
           // if error in dev mode
           notify.onError({
-            title: 'HTML',
-            message: 'Error: <%= error.message %>',
+            title: "HTML",
+            message: "Error: <%= error.message %>",
           })(error);
 
           // if error in production mode
@@ -53,23 +54,24 @@ const html = () => {
             process.exit(1);
           }
         },
-      })
+      }),
     )
     .pipe(data(getDataForFile))
     .pipe(
       nunjucksRender({
-        path: ['src/html/partials'],
+        path: ["src/html/partials"],
         manageEnv: manageEnvironment, // set up the environment using the additional filter
         envOptions: {
           autoescape: false,
         },
         watch: true,
-      })
+      }),
     )
     .pipe(mode.production(htmlmin(htmlminConfig)))
     .pipe(mode.production(cachebust(cachebustConfig)))
-    .pipe(mode.production(rename({ extname: '.php' })))
+    .pipe(mode.production(rename({ extname: ".php" })))
     .pipe(gulp.dest(paths.build.html));
 };
 
 module.exports = { html };
+
