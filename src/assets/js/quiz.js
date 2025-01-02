22912a2416c6b2b10154242710a1ca066d10d449
Yulchikss <yuliapelia19@gmail.com>
2025-01-02 13:07:03 +0200

    feat: adapt the final quiz page with a form for all device resolutions and add dynamic form display
    - Adapted the final quiz page form for all device resolutions (mobile, tablet, laptop, desktop).
    - Added functionality to dynamically display the appropriate form based on user answers.
    - Optimized the logic for determining categories and updating the UI for each type.

--- src/assets/js/quiz.js
+++ src/assets/js/quiz.js
@@ -16,14 +16,82 @@ export const refs = {
   countQuestionSpan: quizWrapper?.querySelector('.count-question-span'),
 };
 
+const categoryConfig = {
+  Python: {
+    countsKey: 'Python',
+    benefits: [
+      "<b>На першому уроці</b> ваша дитина створить телеграм-бота",
+      "<b>На курсі</b> вивчатиме основи Python, автоматизацію процесів, аналіз даних",
+      "<b>Перспективи:</b> створення власних програм, аналіз даних, розробка штучного інтелекту"
+    ],
+    backgrounds: {
+      small: './assets/images/sections/quiz/python-bg.webp',
+      medium: './assets/images/sections/quiz/python-bg-tab.webp',
+      large: './assets/images/sections/quiz/python-bg-lap.webp',
+      extraLarge: './assets/images/sections/quiz/python-bg-desk.webp'
+    },
+    title: 'Ідеальний напрям для вашої дитини — <br class="sm:hidden"> Python!',
+    subtitle: 'Записатися на безоплатний урок з Python'
+  },
+  Design: {
+    countsKey: 'Design',
+    benefits: [
+      "<b>На першому уроці</b> ваша дитина створить листівку, банер або GIF-анімацію",
+      "<b>На курсі</b> вивчатиме роботу у Figma, основи цифрового дизайну та брендингу",
+      "<b>Перспективи:</b> професії графічного дизайнера, ілюстратора, UX/UI-спеціаліста"
+    ],
+    backgrounds: {
+      small: './assets/images/sections/quiz/design-bg.webp',
+      medium: './assets/images/sections/quiz/design-bg-tab.webp',
+      large: './assets/images/sections/quiz/design-bg-lap.webp',
+      extraLarge: './assets/images/sections/quiz/design-bg-desk.webp'
+    },
+    title: 'Ідеальний напрям для вашої дитини — <br class="sm:hidden"> Design!',
+    subtitle: 'Записатися на безоплатний урок з Design'
+  },
+  Frontend: {
+    countsKey: 'Frontend',
+    benefits: [
+      "<b>На першому уроці</b> ваша дитина створить власний простий сайт",
+      "<b>На курсі</b> і вивчатиме HTML, CSS, JavaScript та основи сучасних фреймворків",
+      "<b>Перспективи:</b> кар'єра веброзробника, full-stack інженера, frontend-розробника"
+    ],
+    backgrounds: {
+      small: './assets/images/sections/quiz/frontend-bg.webp',
+      medium: './assets/images/sections/quiz/frontend-bg-tab.webp',
+      large: './assets/images/sections/quiz/frontend-bg-lap.webp',
+      extraLarge: './assets/images/sections/quiz/frontend-bg-desk.webp'
+    },
+    title: 'Ідеальний напрям для вашої дитини — <br class="sm:hidden"> Frontend!',
+    subtitle: 'Записатися на безоплатний урок з Frontend'
+  },
+  GameDev: {
+    countsKey: 'GameDev',
+    benefits: [
+      "<b>На першому уроці</b> ваша дитина створить математичну гру-угадайку",
+      "<b>На курсі</b> вивчатиме Unity, C#, 2D та 3D дизайн ігор",
+      "<b>Перспективи:</b> кар'єра розробника ігор, ігрового дизайнера, власника ігрової студії"
+    ],
+    backgrounds: {
+      small: './assets/images/sections/quiz/game-dev-bg.webp',
+      medium: './assets/images/sections/quiz/game-dev-bg-tab.webp',
+      large: './assets/images/sections/quiz/game-dev-bg-lap.webp',
+      extraLarge: './assets/images/sections/quiz/game-dev-bg-desk.webp'
+    },
+    title: 'Ідеальний напрям для вашої дитини — <br class="sm:hidden"> GameDev!',
+    subtitle: 'Записатися на безоплатний урок з GameDev'
+  }
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
   let currentStep = 0;
   let answersString = '';
   const counts = {
-    python: 0,
-    design: 0,
-    frontend: 0,
-    game: 0
+    Python: 0,
+    Design: 0,
+    Frontend: 0,
+    GameDev: 0
   };
   const updateQuizStep = () => {
     refs?.quizItems?.forEach((item, index) => {
@@ -74,47 +142,47 @@ document.addEventListener('DOMContentLoaded', () => {
     window.quizAnswers = answersString;
   };
 
+  const updateBackground = (imageSet) => {
+    if (window.innerWidth < 768) {
+      return imageSet.small;
+    } else if (window.innerWidth < 1280) {
+      return imageSet.medium;
+    } else if (window.innerWidth < 1600) {
+      return imageSet.large;
+    } else {
+      return imageSet.extraLarge;
+    }
+  }
+
+  const updateUI = (categoryKey, refs) => {
+    const config = categoryConfig[categoryKey];
+    refs.sectionStepThree.style.backgroundImage = `url(${updateBackground(config.backgrounds)})`;
+    refs.formTitle.innerHTML = config.title;
+    refs.formSubtitle.textContent = config.subtitle;
+    refs.formList.innerHTML = config.benefits
+      .map(benefit => `<li class='item'><svg class='flex-shrink-0' width="24" height="24" aria-label="іконка відмітки"><use href="./assets/images/sprite.svg#done-icon"></use></svg> <p>${benefit}</p></li>`)
+      .join('');
+  }
+
+
   refs.quizItems.forEach((item, index) => {
     const radios = item.querySelectorAll('input[type="radio"]');
     radios.forEach(radio => {
       radio.addEventListener('change', () => {
         console.log(radio.parentElement.dataset.id);
         const category = radio.parentElement.dataset.id;
-        refs.sectionStepOne.classList.add('min-h-[1140px]');
-        refs.sectionStepOne.classList.remove('min-h-[888px]');
+        refs.sectionStepOne.classList.add('min-h-[1140px]', "md:min-h-[960px]", "xl:min-h-[80vh]");
+        refs.sectionStepOne.classList.remove('min-h-[888px]', "md:h-[1344px]", "xl:h-[80vh]");
 
-        if(category === 'Python') {
-          counts.python++;
-        } else if(category === 'Design') {
-          counts.design++;
-        } else if(category === 'Frontend') {
-          counts.frontend++;
-        } else if(category === 'Game') {
-          counts.game++;
-        }
-        const arrayValues = Object.values(counts);
-        const maxValue = Math.max(...arrayValues);
+        counts[categoryConfig[category].countsKey]++;
+        const maxCategory = Object.keys(counts).reduce((a, b) => counts[a] > counts[b] ? a : b);
+        updateUI(maxCategory, refs);
 
-        if (maxValue === counts.python) {
-          const benefits =  [
-            "<b>На першому уроці</b> ваша дитина створить телеграм-бота",
-            "<b>На курсі</b> вивчатиме основи Python, автоматизацію процесів, аналіз даних",
-            "<b>Перспективи:</b> створення власних програм, аналіз даних, розробка штучного інтелекту"
-          ]
+        console.log(maxCategory);
 
-          refs.sectionStepTwo.style.backgroundImage = 'url(./assets/images/quiz/python-bg.jpg)';
-          refs.formTitle.innerHTML = 'Ідеальний напрям для вашої дитини — <br class="sm:hidden"> Python!';
-          refs.formSubtitle.textContent = 'Записатися на безоплатний урок з Python';
-          refs.formList.innerHTML = benefits.map(benefit => `<li class='item'><svg class='flex-shrink-0' width="24" height="24" aria-label="іконка відмітки"><use href="./assets/images/sprite.svg#done-icon"></use></svg> <p> ${benefit}</p></li>`).join('');
-
-          console.log( ' choose python');
-        } else if (maxValue === counts.design) {
-          console.log('choose design');
-        } else if (maxValue === counts.frontend) {
-          console.log(' choose frontend');
-        } else if (maxValue === counts.game) {
-          console.log(' choose game');
-        }
+        window.addEventListener('resize', () => {
+          refs.sectionStepThree.style.backgroundImage = `url(${updateBackground(categoryConfig[maxCategory].backgrounds)})`;
+        });
 
 
         console.log(counts);
@@ -135,12 +203,12 @@ document.addEventListener('DOMContentLoaded', () => {
   // Export the answers string
   window.quizAnswers = answersString;
 
- refs.quizItems.forEach((item, index) => {
-  item.addEventListener('click', (e) => {
-    if (e.target.classList.contains('back-btn')) {
-      currentStep--;
-      updateQuizStep();
-    }
+  refs.quizItems.forEach((item, index) => {
+    item.addEventListener('click', (e) => {
+      if (e.target.classList.contains('back-btn')) {
+        currentStep--;
+        updateQuizStep();
+      }
+    })
   })
- })
 });
