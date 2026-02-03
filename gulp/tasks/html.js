342cd3616a50a70b2af593d118ed1b7716845120
Yulia <yuliapelia19@gmail.com>
2026-02-03 14:50:23 +0200

    updated logo

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -32,8 +32,14 @@ const manageEnvironment = function (environment) {
   });
 };
 
-const getDataForFile = (file) => {
-  return JSON.parse(fs.readFileSync("./src/json/data.json"));
+const getDataForFile = file => {
+  const data = JSON.parse(fs.readFileSync('./src/json/data.json'));
+  // Cache bust для спрайту — gulp-cache-bust не чіпає SVG <use href>
+  const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+  data.assetsVersion = process.argv.includes('--production')
+    ? `${pkg.version}-${Date.now()}`
+    : 'dev';
+  return data;
 };
 
 const html = () => {
